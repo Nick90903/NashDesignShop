cc3cb13928142c5d20fbbfe3135dbed6f14f5da4
root <root@DESKTOP-I9HG4JT.localdomain>
2022-10-22 20:25:39 -0500

    Updated Main and adding pricing page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,6 +6,23 @@ function LoadMain () {
     return (
         <div id="Main">
             <RenderHeader />
+            <div className="body">
+                <div className="p1">
+                    <h2>About Us</h2>
+                    <div className="paragraph">
+                        <p>We are a small 3D printing and modeling company based
+                        in Eastern Wisconsin. We use only the best filaments and
+                        custom 3D printers setup specially to provide high quality
+                        and high strength parts</p>
+                        <p>Our main goal is to provide the best service for people
+                        who need small batch, custom, parts. Most of what we do is
+                        parts for the FPV and RC community.</p>
+                        <p>For some ideas on what we make and the quality we provide
+                        check out our <a href="">Gallery</a> and for pricing you can
+                        use our pricing calculator <a href="">here</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/Components/Pricing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadPricing() {
+    return (
+        <div id="Pricing">
+            <div id="calculator">
+
+            </div>
+        </div>
+    );
+}
+
+export default LoadPricing
