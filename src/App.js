5e988f90b49c0d07ec76be200c2d0c411876f2da
root <root@DESKTOP-I9HG4JT.localdomain>
2022-10-27 06:09:11 -0500

    Added layout for Calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LoadExample from './Components/Examples';
 import LoadMain from './Components/Main';
+import LoadPricing from './Components/Pricing';
 
 function App() {
   return (
     <div className="App">
-      <LoadMain />
+      <LoadPricing />
     </div>
   );
 }
--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,10 +1,31 @@
 import React from "react";
-
+import "../Styles/Pricing.css"
 function LoadPricing() {
     return (
         <div id="Pricing">
             <div id="calculator">
-
+                <div className="weight-Container  input-container">
+                    <label htmlFor="weight">Weight: </label>
+                    <input type="number" id="weight"></input>
+                </div>
+                <div className="time-Container  input-container">
+                    <label htmlFor="time">Time: </label>
+                    <input type="number" id="time"></input>
+                </div>
+                <div className="cost-weight-Container  input-container">
+                    <label htmlFor="cost-weight">Cost weight: </label>
+                    <input type="number" id="cost-weight"></input>
+                </div>
+                <div className="cost-time-Container  input-container">
+                    <label htmlFor="cost-time">Cost time: </label>
+                    <input type="number" id="cost-time"></input>
+                </div>
+                <div className="button-Container">
+                    <button id="submit">Submit</button>
+                </div>
+            </div>
+            <div className="outputs">
+                <div className="final-Cost"></div>
             </div>
         </div>
     );
